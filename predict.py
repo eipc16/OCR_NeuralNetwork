45f6d3c8607b98718f2e969ef77b77ab3fa84ec7
eipc16 <pprzemek.312@gmail.com>
2018-04-13 16:47:43 +0200

    Modified predict file to not include test_samples in training

--- predict.py
+++ predict.py
@@ -12,7 +12,7 @@ def predict(x):
     x_train, y_train = pkl.load(open('hog_train.pkl', mode='rb'))
     nn = NeuralNetwork([x_train.shape[1], 384, y_train.shape[1]])
     print('Starting training with test sample size: ' + str(hog_test_x.shape[0]) + ' and number of iterations: ' + str(num_epochs))
-    nn.fit(x_train, y_train, epochs=num_epochs)
+    nn.fit(x_train[10000:], y_train[10000:], epochs=num_epochs)
     print('Training has been successfully completed.')
     #nn.setweights(pkl.load(open('NN_weights.pkl')))
 
